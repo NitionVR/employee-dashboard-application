dc64125b85eae420ba94d9926fbfd145b19e7733
NitionVR <kmaganyane83@gmail.com>
2025-01-18 09:00:44 +0200

    Add project structure and basic configuration

---
+++ src/main/java/com/employee/EmployeeManagementApplication.java
@@ -0,0 +1,12 @@
+package com.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// src/main/java/com/employee/EmployeeManagementApplication.java
+@SpringBootApplication
+public class EmployeeManagementApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeManagementApplication.class, args);
+    }
+}
